d8177ca4fd8acdb5e6a00e2253266646b0f5da1f
Ryan <ryan_cordial.it@wilcon.com.ph>
2025-03-17 09:55:43 +0800

    Add project files.

---
+++ MyApp/Controllers/ItemsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyApp.Models;
+
+namespace MyApp.Controllers
+{
+    public class ItemsController : Controller
+    {
+        public IActionResult Overview()
+        {
+            var item = new Item() { Name = "Keyboard" };
+            return View(item);
+        }
+
+        public IActionResult Edit(int id) {
+            return Content($"ID = {id}");
+        }
+    }
+}
---
+++ MyApp/Data/MyAppContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyApp.Models;
+
+namespace MyApp.Data
+{
+    public class MyAppContext : DbContext
+    {
+        //Constructor
+        public MyAppContext(DbContextOptions<MyAppContext> options) : base(options) { }
+
+        //Set Auto Generate Table Schema to SQL Server Management
+        public DbSet<Item> Items { get; set; }
+    }
+}
