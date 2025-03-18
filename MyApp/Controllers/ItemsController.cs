2d8aae218b394173c69a61677aca04cc70ddb111
Ryan Cordial <ryancordial.job@gmail.com>
2025-03-18 16:32:44 +0800

    Added CRUD except delete

--- MyApp/Controllers/ItemsController.cs
+++ MyApp/Controllers/ItemsController.cs
@@ -1,18 +1,86 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyApp.Data;
 using MyApp.Models;
 
 namespace MyApp.Controllers
 {
     public class ItemsController : Controller
     {
-        public IActionResult Overview()
+        //Initalize DB Context
+        protected readonly MyAppContext _context;
+
+        public ItemsController(MyAppContext context)
         {
-            var item = new Item() { Name = "Keyboard" };
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var items = await _context.Items.ToListAsync();
+            return View(items);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create([Bind("Name, Price")] Item item) {
+
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                     _context.Items.Add(item);
+                    _context.SaveChanges();
+
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex) {
+                ModelState.AddModelError("", ex.Message);
+            }
             return View(item);
         }
 
-        public IActionResult Edit(int id) {
-            return Content($"ID = {id}");
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var item = await _context.Items.FirstOrDefaultAsync(x => x.Id == id);
+
+            Console.WriteLine(item);
+            return View(item);
         }
+
+        [HttpPost, ActionName("Update")]
+        public async Task<IActionResult> Edit(int id, [Bind("Id, Name, Price")] Item item)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _context.Items.Update(item);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", ex.Message);
+            }
+            return View(item);
+        }
+
+        //public IActionResult Overview()
+        //{
+        //    var item = new Item() { Name = "Keyboard" };
+        //    return View(item);
+        //}
+
+        //public IActionResult Edit(int id) {
+        //    return Content($"ID = {id}");
+        //}
     }
 }
--- MyApp/Models/Item.cs
+++ MyApp/Models/Item.cs
@@ -3,6 +3,7 @@
     public class Item
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string Name { get; set; } = null!;
+        public double Price { get; set; }
     }
 }
